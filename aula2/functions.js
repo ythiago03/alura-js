6847fd1ca59496e964c79b7d6567297c10336b6e
Thiago Henrique <92443688+ythiago03@users.noreply.github.com>
2022-03-08 12:13:56 -0300

    Add files via upload

---
+++ aula2/functions.js
@@ -0,0 +1,33 @@
+function converter(s){
+    var money = Number(document.getElementById('valor').value)
+    var real 
+    switch (s) {
+        case 1:
+            real = (money * 5.08).toFixed(2)//dolar
+
+            break;
+
+        case 2:
+            real = (money * 5.55).toFixed(2)//euro
+                break;
+
+        case 3:
+            real = (money *0.044).toFixed(2)//iene japonês
+            break;
+
+        case 4:
+            real = (money * 6.69).toFixed(2)//libra esterlina britânica
+            break;
+        
+        default:
+        
+            break;          
+    }
+    var res = document.getElementById('valorConvertido')
+    res.innerHTML = ` A conversão para real é: R$${real} `
+}
+
+   /* var soma = (money * 5.08).toFixed(2)
+    var res = document.getElementById('valorConvertido')
+    res.innerHTML = ` A conversão de R$${real} para dolar é: $${soma}`*/
+
