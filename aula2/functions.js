84413629b77cbd21c67e41827485e731fc8f3a4d
Thiago Henrique <92443688+ythiago03@users.noreply.github.com>
2022-03-09 08:34:32 -0300

    Update functions.js

--- aula2/functions.js
+++ aula2/functions.js
@@ -1,30 +1,31 @@
 function converter(s){
     var money = Number(document.getElementById('valor').value)
     var real 
+    var res = document.getElementById('valorConvertido')
     switch (s) {
         case 1:
             real = (money * 5.08).toFixed(2)//dolar
-
+            res.innerHTML = ` A conversão de $${money} para real é: R$${real} `
             break;
 
         case 2:
             real = (money * 5.55).toFixed(2)//euro
+            res.innerHTML = ` A conversão de €${money} para real é: R$${real} `
                 break;
 
         case 3:
-            real = (money *0.044).toFixed(2)//iene japonês
+            real = (money * 0.044).toFixed(2)//iene japonês
+            res.innerHTML = ` A conversão de ¥${money} para real é: R$${real} `
             break;
 
         case 4:
             real = (money * 6.69).toFixed(2)//libra esterlina britânica
+            res.innerHTML = ` A conversão de £${money} para real é: R$${real} `
             break;
         
         default:
         
             break;          
     }
-    var res = document.getElementById('valorConvertido')
-    res.innerHTML = ` A conversão para real é: R$${real} `
+    
 }
-
-   
