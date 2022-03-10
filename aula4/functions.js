aa69c8e1b8f996ae3187abbe316fc3c5d57ad914
Thiago Henrique <92443688+ythiago03@users.noreply.github.com>
2022-03-10 16:21:23 -0300

    Add files via upload

---
+++ aula4/functions.js
@@ -0,0 +1,20 @@
+
+
+/*===== animes =====*/
+
+var animes = ['https://cdn.fstatic.com/media/movies/covers/2019/12/tWJRkoAMGTLLT8q8IIahJWpvb1n_Ov6ZjP6.jpg',
+'https://i0.wp.com/www.otakupt.com/wp-content/uploads/2021/01/Mushoku-Tensei-visual-poster.jpg?resize=696%2C986&ssl=1',
+'https://img.elo7.com.br/product/original/1EE36C0/big-poster-anime-re-zero-tamanho-90x-0-cm-lo003-poster.jpg','https://images-na.ssl-images-amazon.com/images/I/51WvxghcgpL._SX329_BO1,204,203,200_.jpg',
+'http://vortexcultural.com.br/images/2021/05/jujutsu_capa.png',]
+
+
+for (let i = 0; i < animes.length; i++ ){
+    document.write("<img  src=" + animes[i] + " class='images'>")
+}
+
+
+/* ======= series ========*/
+
+
+
+
